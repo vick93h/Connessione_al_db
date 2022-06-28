168057d71de1150ec26e5edac3212124fe031de2
Vittorio <Vittorio@iMacdiVittorio.homenet.telecomitalia.it>
2022-06-28 16:37:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+
+
+
+    }
+}
+
